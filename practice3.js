a5b9e17fc8aace04b25eae8276a9a772d912c616
Evgeniy <yoshinakakira@gmail.com>
2024-06-10 19:17:22 +0300

    practice3

---
+++ practice3.js
@@ -0,0 +1,88 @@
+// let weather = 'гроза'; // дождь, ветренно, снег, гроза
+
+// if (weather === 'дождь') {
+//     console.log('Ужас, а зонтик у нас собой?')
+// }
+
+// if (weather === 'ветренно') {
+//     console.log('Нужно быть аккуратнее, шарфик должен быть при нас!')
+// }
+
+// if (weather === 'снег') {
+//     console.log('Чтобы не заболеть необходимо взять с собой шарфик и шапку!')
+// }
+
+// if (weather === 'гроза') {
+//     console.log('В такую погоду лучше оставаться дома!')
+// }
+
+
+// let weather = 'дождь';
+
+// let isRain = weather === 'дождь' // заранее вычислим есть дождь или нет
+// if(isRain){
+//     console.log('Не забудь взять с собой дождь')
+// }
+
+
+// let userAge = 24;
+
+// let isCurrentAge = userAge === 24; // заранее вычислим есть число 24 и результатом будет true/false
+
+// if(isCurrentAge){
+//     console.log('вам 24 года')
+// }
+
+// let userName = 'Evgeniy';
+    
+// if (userName) { // true
+//         console.log('У переменной есть значение')
+//     }
+
+// let userName = null;
+//     console.log(Boolean(userName))
+//     if(userName) {
+//         console.log('Привет, меня зовут' + userName)
+//     }
+
+// let age = 29 // Обьявили переменную age и присвоили ей значение 24 
+
+// let isAdult = age >= 24; // Объявили переменную isAdult, присвоили ей значение переменной age и провели сравнение 
+// console.log(Boolean(isAdult)) // true / false в зависимости от результата
+// let ageCategory = isAdult ? 'Взрослый' : 'Ребёнок' // true === 'Взрослый' false === 'Ребёнок'
+// console.log(ageCategory)
+
+
+// let score = 20;
+// let grade = score >= 90;
+// if(grade) {
+//     console.log('Grade A')
+// }else{
+//     console.log('Вы не набрали нужно кол-во баллов')
+// }
+
+// console.log(grade)
+
+// let score = 76;
+// let grade = score // Переменная grade присвоила значение переменной score (90)
+// if(grade >= 90){
+//     console.log('Grade A')
+// }else if(grade > 75){
+//     console.log('Grade B')
+// }else if(grade > 60){
+//     console.log('Grade C')
+// }else{
+//     console.log('Grade D')
+// }
+
+
+// let score = 14;
+// let grade = score >= 90 ? 'Grade A' : score >=75 ? 'Grade B' : score > 60 ? 'Grade C' : 'Grade D';
+
+// console.log(grade)
+
+// let age = 15;
+// let checkAge = age >= 25
+// let checkAdult = checkAge ? 'Взрослый' : 'Ребёнок'; // если true то возвращается первое выражение до : А если false то после знака :
+
+// console.log(checkAdult) 
